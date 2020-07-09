ece3a319f1cee5d9740145aa0778347ee3ac4ec2
Steve <ycsteven.tsao@gmail.com>
2020-07-10 06:39:09 +0800

    修改中

--- js/app.js
+++ js/app.js
@@ -4,7 +4,12 @@ var apiPath = "https://course-ec-api.hexschool.io/";
 var productList = document.querySelector("#productList");
 
 var productData = {
-  data: {},
+  data: {
+    uuid = "5bffb293-5936-4139-961e-e2006317c701",
+    products:[],
+  },
 
-  getData() {},
+  getData() {
+    var vm=this;
+  },
 };
